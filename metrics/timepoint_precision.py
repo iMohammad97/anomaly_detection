422755429f6065502281888be16d61ca76cfb3b8
MoHammad Mohammadi <mohammadm97i@gmail.com>
2024-12-31 17:29:44 +0330

    Adds pointwise_precision to metrics.

---
+++ metrics/timepoint_precision.py
@@ -0,0 +1,8 @@
+import numpy as np
+from sklearn.metrics import precision_score
+
+def pointwise_precision(y_true, y_pred):
+    """
+    Timepoint-wise precision: fraction of detected anomalies that are correct.
+    """
+    return precision_score(y_true, y_pred)
