57fabe562647f35f5ebdab60825ec53b113053ab
sahadi1375 <160831337+sahadi1375@users.noreply.github.com>
2025-01-02 23:42:35 +0330

    Update synchronized_TS_visualization.py

--- synchronized_TS_visualization.py
+++ synchronized_TS_visualization.py
@@ -5,18 +5,36 @@ import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 
 def plot_sync_series(time_series, title="Synchronized Time Series Plots"):
-    """Helper function to visualize multiple time series as separate subplots."""
-    # Create subplots
+
     fig = make_subplots(rows=len(time_series), cols=1, shared_xaxes=True)
 
-    # Adding time series to subplots
+    labels = time_series.pop('labels', None)
+
     i = 1
     for key, value in time_series.items():
-        fig.add_trace(go.Scatter(x=value.index, y=value.values.reshape(-1), mode='lines', name=f'{key}'), row=i, col=1)
-        i += 1
+      fig.add_trace(go.Scatter(
+          x=value.index, 
+          y=value.values.reshape(-1), 
+          mode='lines', 
+          name=f'{key}'
+      ), row=i, col=1)
 
+      if labels.any():  # Ensure there are labels to plot
+          # Filter for points where labels are 1
+          scatter_points = labels[labels == 1].index
+          scatter_values = value[scatter_points].values.reshape(-1)
+          fig.add_trace(go.Scatter(
+              x=scatter_points, 
+              y=scatter_values, 
+              mode='markers', 
+              name=f'{key} - Labels'
+          ), row=i, col=1)
+      i += 1
     # Update layout for better visibility and interaction
-    fig.update_layout(height=300 * len(time_series), width=1500, title_text=title)
+    fig.update_layout(
+        height=300 * len(time_series), 
+        width=1500, 
+        title_text=title
+    )
 
-    # Plot the figure
     fig.show()
