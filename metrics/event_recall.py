038c19afcb13c908cd91bfc775f60f9720ac89d1
sahadi1375 <160831337+sahadi1375@users.noreply.github.com>
2025-01-03 19:24:22 +0330

    Update event_recall.py

--- metrics/event_recall.py
+++ metrics/event_recall.py
@@ -1,3 +1,19 @@
+def make_event(y_true, y_pred):
+    
+    y_true_starts = np.argwhere(np.diff(y_true.flatten(), prepend=0) == 1).flatten()
+    y_true_ends   = np.argwhere(np.diff(y_true.flatten(), append=0) == -1).flatten()
+    y_true_events = list(zip(y_true_starts, y_true_ends))
+
+    y_pred_starts = np.argwhere(np.diff(y_pred, prepend=0) == 1).flatten()
+    y_pred_ends   = np.argwhere(np.diff(y_pred, append=0) == -1).flatten()
+    y_pred_events = list(zip(y_pred_starts, y_pred_ends))
+
+    return y_true_events,y_pred_events
+
+
+
+
+
 def event_wise_recall(y_true_events, y_pred_events):
     """
     Event-based recall. We consider an event 'detected' if the predicted event
