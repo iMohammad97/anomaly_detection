b81cd387cebf61c98073e3022316d75748dbe987
MoHammad Mohammadi <mohammadm97i@gmail.com>
2025-01-07 12:47:18 +0330

    Adds demonstration of how to call preprocess_UCR in different to test_main.py.

--- utilities/test_main.py
+++ utilities/test_main.py
@@ -1,9 +1,16 @@
+from utilities.preprocess import preprocess_UCR
+from utilities.visualize import visualizeUCR
+
 if __name__ == '__main__':
-    raw_input_dir = "../UCR/UCR_TimeSeriesAnomalyDatasets2021/FilesAreInHere/UCR_Anomaly_FullData"
-    input_dir = "../UCR/UCR2_preprocessed"
-    output_dir = "report.pdf"
-    output_pdfs_path = "./anomaly_types"
-    anomalies_type_csv_path = "anomaly_types.csv"
+
+    ##################################################
+    ############## To call visualizeUCR ##############
+    ##################################################
+    # raw_input_dir = "../UCR/UCR_TimeSeriesAnomalyDatasets2021/FilesAreInHere/UCR_Anomaly_FullData"
+    # input_dir = "../UCR/UCR2_preprocessed"
+    # output_dir = "report.pdf"
+    # output_pdfs_path = "./anomaly_types"
+    # anomalies_type_csv_path = "anomaly_types.csv"
 
     # mode=0, plots all
     # visualizeUCR(input_dir, output_dir)
@@ -18,3 +25,14 @@ if __name__ == '__main__':
     # mode=3, plots all types of anomalies, each in a different pdf file
     # visualizeUCR(input_dir, output_dir, mode=3, anomalies_type_csv=anomalies_type_csv_path,
     #              output_pdfs_path=output_pdfs_path)
+
+
+
+
+    ##################################################
+    ############ To call preprocess_UCR ##############
+    ##################################################
+    input_dir = "../UCR/UCR_TimeSeriesAnomalyDatasets2021/FilesAreInHere/UCR_Anomaly_FullData"
+    output_dir = "../UCR/UCR2_preprocessed"
+
+    preprocess_UCR(input_dir, output_dir)
