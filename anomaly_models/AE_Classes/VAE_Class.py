3b243feefae9af695e346f5473382f7fc01fdf04
sahadi1375 <160831337+sahadi1375@users.noreply.github.com>
2025-01-09 14:31:23 +0330

    Update VAE_Class.py

--- anomaly_models/AE_Classes/VAE_Class.py
+++ anomaly_models/AE_Classes/VAE_Class.py
@@ -33,11 +33,11 @@ class VariationalLSTMAutoencoder:
         class Sampling(layers.Layer):
           """Uses (z_mean, z_log_var) to sample z, the vector encoding a digit."""
             def call(self, inputs):
-              z_mean, z_log_var = inputs
-              batch = tf.shape(z_mean)[0]
-              dim = tf.shape(z_mean)[1]
-              epsilon = tf.keras.backend.random_normal(shape=(batch, dim))
-              return z_mean + tf.exp(0.5 * z_log_var) * epsilon
+                z_mean, z_log_var = inputs
+                batch = tf.shape(z_mean)[0]
+                dim = tf.shape(z_mean)[1]
+                epsilon = tf.keras.backend.random_normal(shape=(batch, dim))
+                return z_mean + tf.exp(0.5 * z_log_var) * epsilon
         
         # Encoder
         inputs = tf.keras.Input(shape=(timesteps, features))
