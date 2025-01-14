c4b37282fdc82836202af06643867d0cd59610de
sahadi1375 <160831337+sahadi1375@users.noreply.github.com>
2025-01-14 12:09:17 +0330

    Update auc_event.py

--- metrics/auc_event.py
+++ metrics/auc_event.py
@@ -1,16 +1,9 @@
-import numpy as np
-from sklearn.metrics import auc
-import matplotlib.pyplot as plt
-from .timepoint_precision import pointwise_precision
-from .event_recall import event_wise_recall, make_event
-
-def custom_auc_score(y_true, anomaly_scores, threshold_steps=100, plot=False):
-
-
+def custom_auc_with_perfect_point(y_true, anomaly_scores, threshold_steps=100, plot=False):
     # Generate thresholds using percentiles
     percentiles = np.linspace(np.min(anomaly_scores), np.max(anomaly_scores) + 1e-7, threshold_steps)
     precision_list = []
     recall_list = []
+    perfect_point_found = False
 
     for threshold in percentiles:
         # Convert anomaly scores to binary predictions based on threshold
@@ -27,6 +20,11 @@ def custom_auc_score(y_true, anomaly_scores, threshold_steps=100, plot=False):
         precision_list.append(prt)
         recall_list.append(rece)
 
+        # Check if both precision and recall are 1
+        if prt == 1 and rece == 1:
+            perfect_point_found = True
+            break
+
     # Compute AUC using precision-recall pairs
     custom_auc = auc(recall_list, precision_list)
 
@@ -41,4 +39,4 @@ def custom_auc_score(y_true, anomaly_scores, threshold_steps=100, plot=False):
         plt.legend(loc="best")
         plt.show()
 
-    return custom_auc
+    return custom_auc, perfect_point_found
